62a1215c008eb50ad8320650fdfc0e0691b8915b
Angshuman Sengupta <angshuman@cse.iitb.ac.in>
2024-10-27 15:54:51 +0530

    Removed unwanted files

--- Components/src/App.js
+++ Components/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import CartIcon from './components/CartIcon';
 import ProductList from './components/ProductList';
 import products from './productList'
 
@@ -12,7 +11,6 @@ function App() {
         <main>
           <ProductList products={products}/>
         </main>
-        <CartIcon itemCount={0} />
       <Footer />
     </div>
   );
